7b4cf54d5e63ff07f0b0894cfa85a698ab4bc08c
ZuLu29 <markneilconde850@gmail.com>
2024-05-24 23:14:18 +0800

    adding create functions

--- api.py
+++ api.py
@@ -31,5 +31,35 @@ def get_employees():
     return make_response(jsonify(data), 200)
 
 
+@app.route("/employees", methods=["POST"])
+def add_employees():
+    cur = mysql.connection.cursor()
+    info = request.get_json()
+    ssn = info["ssn"]
+    Fname = info["Fname"]
+    Minit = info["Minit"]
+    Lname = info["Lname"]
+    Bdate = info["Bdate"]
+    Address = info["Adress"]
+    Sex = info["Sex"]
+    Salary = info["Salary"]
+    Super_ssn = info["Super_ssn"]
+    DL_id = info["DL_id"]
+    cur.execute(
+        """ INSERT INTO actor (ssn, Fname, Minit, Lname, Bdate, Adress, Sex, Salary, Super_ssn, DL_id) VALUE (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",
+        (ssn, Fname, Minit, Lname, Bdate, Address, Sex, Salary, Super_ssn, DL_id),
+    )
+    mysql.connection.commit()
+    print("row(s) affected :{}".format(cur.rowcount))
+    rows_affected = cur.rowcount
+    cur.close()
+    return make_response(
+        jsonify(
+            {"message": "actor added successfully", "rows_affected": rows_affected}
+        ),
+        201,
+    )
+
+
 if __name__ == "__main__":
     app.run(debug=True)
